433746e5344e13e79870f5e5b053d1f8df568f36
bbigbear <citizenjohn.superdev@gmail.com>
2023-08-15 05:34:34 +0800

    added backend url to cors configs

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000","https://eshopify-online-store.onrender.com/"],
   credentials: true
 }));
 
