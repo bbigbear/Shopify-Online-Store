bb1617f2b58fbf20b31ba6166402a5e828a6a5be
bbigbear <citizenjohn.superdev@gmail.com>
2023-08-15 05:36:05 +0800

    removed trailing slash from endpoint

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:3000","https://eshopify-online-store.onrender.com/"],
+  origin: ["http://localhost:3000","https://eshopify-online-store.onrender.com"],
   credentials: true
 }));
 
