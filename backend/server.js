57fa65175c90c9daddd4faacdd5bde9cde370e21
bbigbear <citizenjohn.superdev@gmail.com>
2023-08-15 14:16:36 +0800

    test commit

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(
     saveUninitialized: false,
     cookie: { 
       maxAge: 24 * 60 * 60 * 1000, 
-      Secure: true,
+      secure: true,
       httpOnly: true,
       sameSite: 'none'
     },
