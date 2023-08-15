b5a15f88be15323b82b5a05cdb830177b659af39
bbigbear <citizenjohn.superdev@gmail.com>
2023-08-15 13:44:32 +0800

    fixed cookie options

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(
     saveUninitialized: false,
     cookie: { 
       maxAge: 24 * 60 * 60 * 1000, 
-      secure: envType === 'PRODUCTION', 
+      secure: true, 
       sameSite: 'none'
     },
   })
