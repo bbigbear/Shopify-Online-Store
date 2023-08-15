8e289d5db807ce3305d291f08743bf149f669b8a
bbigbear <citizenjohn.superdev@gmail.com>
2023-08-15 13:59:36 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,8 @@ app.use(
     saveUninitialized: false,
     cookie: { 
       maxAge: 24 * 60 * 60 * 1000, 
-      secure: process.env.ENVIRONMENT === 'PRODUCTION', 
+      secure: process.env.ENVIRONMENT === 'PRODUCTION',
+      sameSite: 'none'
     },
   })
 );
