9ef71e5e8db0009086017a392aff59199f70b09d
bbigbear <citizenjohn.superdev@gmail.com>
2023-08-15 12:32:03 +0800

    changed endpoint url

--- frontend/src/utils/endPoint.js
+++ frontend/src/utils/endPoint.js
@@ -1,2 +1,2 @@
-// export const API_ENDPOINT = 'https://eshopify-online-store.onrender.com/auth';
-export const API_ENDPOINT = 'http://localhost:8000/auth'
+export const API_ENDPOINT = 'https://eshopify-online-store.onrender.com/auth';
+// export const API_ENDPOINT = 'http://localhost:8000/auth'
