f69e6b7904f389336beaccaaf361738cc492b13d
bbigbear <citizenjohn.superdev@gmail.com>
2023-08-15 13:37:28 +0800

    Fixed cors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:3000","https://shopnex.netlify.app","https://eshopify-store.onrender.com/"],
+  origin: ["http://localhost:3000","https://shopnex.netlify.app","https://eshopify-store.onrender.com"],
   credentials: true,
 }));
 
