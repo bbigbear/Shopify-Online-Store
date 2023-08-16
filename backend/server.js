e44b0748c63608d7ae002e43b8361782ef6a749b
bbigbear <citizenjohn.superdev@gmail.com>
2023-08-16 09:17:10 +0800

    changed listening port

--- backend/server.js
+++ backend/server.js
@@ -78,10 +78,11 @@ const authRoutes = require('./routes/routes');
 app.use('/auth', authRoutes);
 
 
-// app.listen(PORT, () => {
-//   console.log("Server is running on port " + PORT);
-// });
-
-server.listen(PORT, () => {
-  console.log(`Server running on https://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
+
+
+// server.listen(PORT, () => {
+//   console.log(`Server running on https://localhost:${PORT}`);
+// });
